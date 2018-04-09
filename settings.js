65e4efbd1d946228dd2a037fada25f58e7dd4e43
danilobenq <danilo.matos@benq.com>
2018-04-09 19:21:00 -0300

    Information Update
    Updating the account information

--- settings.js
+++ settings.js
@@ -1,17 +1,17 @@
 // Change the url to the domain of your app
-exports.url = 'http://localhost:3090';
+exports.url = 'https://bqshopbr.herokuapp.com';
 
-exports.senderEmail = "sender@example.com";
-exports.senderName = "Sender Name";
+exports.senderEmail = "marketing.brasil@benq.com";
+exports.senderName = "Rafaela";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = null;
+exports.listId = 2425980;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
 exports.templateId = null;
 
 // receive an email when a new signup is confirmed
-exports.sendNotification = true;
+exports.sendNotification = false;
 exports.notificationEmail = "admin@example.com";
